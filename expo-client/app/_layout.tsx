adfbde8189de4cdea2eec440a955589ee5808a2f
Crabb <S550248@nwmissouri.edu>
2026-02-03 14:52:08 -0600

    testing other tabs
    tested other tabs "profile" and "reels", autofilled with index.tsx code

--- expo-client/app/_layout.tsx
+++ expo-client/app/_layout.tsx
@@ -3,10 +3,20 @@ import { Tabs } from "expo-router";
 export default function RootLayout() {
   return (
   <Tabs>
+    <Tabs.Screen name="reels" 
+    options={{ 
+      title: "Reels",
+      tabBarLabel: "Reels",
+      }} />
     <Tabs.Screen name="index" 
     options={{ 
       title: "Home",
       tabBarLabel: "Home",
+      }} />
+    <Tabs.Screen name="profile" 
+    options={{ 
+      title: "Profile",
+      tabBarLabel: "Profile",
       }} />
   </Tabs>
 );
