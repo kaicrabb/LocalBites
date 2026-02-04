27b4750226fe454a52f63525ee480c126a372e73
Hayden Bindel <124805187+hbindel26@users.noreply.github.com>
2026-02-04 13:18:44 -0600

    cleaning the tabs
    I edited the tabs to look differently  to notice the changes of switching tabs

--- expo-client/app/profile.tsx
+++ expo-client/app/profile.tsx
@@ -1,46 +1,10 @@
-import React, { useEffect, useRef } from 'react';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { useNavigation } from 'expo-router';
-
-const INITIAL_REGION = {
-  latitude: 40.3589695,
-  longitude: -94.8831951,
-  latitudeDelta: 0.0922,
-  longitudeDelta: 0.0421,
-};
+import { Text, View } from 'react-native';
 
 export default function App() {
-  const mapRef = useRef<MapView>(null);
-
-  const navigation = useNavigation();
-
-  useEffect(() => {
-    navigation.setOptions({
-      headerRight: () => (
-        <TouchableOpacity onPress={focusMap}>
-          <View style={{ padding: 10 }}>
-            <Text>Focus Map</Text>
-          </View>
-        </TouchableOpacity>
-      ),
-    });
-  }, []);
-
-  const focusMap = () => {
-    mapRef.current?.animateToRegion(INITIAL_REGION, 1000);
-  };
-
   return (
-    <View style={{ flex: 1 }}>
-      <MapView 
-      style={StyleSheet.absoluteFill} 
-      provider={PROVIDER_GOOGLE}
-      initialRegion={INITIAL_REGION}
-      showsUserLocation
-      showsMyLocationButton
-      ref={mapRef}
-      />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>profile tab</Text>
     </View>
   );
 }
+
