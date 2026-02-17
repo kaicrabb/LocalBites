3d62fd030d41c2bd720873665cc19a2ada00d1d5
Crabb <S550248@nwmissouri.edu>
2026-02-17 15:13:53 -0600

    set up photo/video cloud storage
    set up firebase cloud storage for photos/videos, need to verify this is working in the application still.

---
+++ expo-client/config/firebaseConfig.tsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDiwARV5sctCT9OSjFNJMmaB5Jkvb47HxQ",
+  authDomain: "localbites-c3696.firebaseapp.com",
+  projectId: "localbites-c3696",
+  storageBucket: "localbites-c3696.firebasestorage.app",
+  messagingSenderId: "1002500513104",
+  appId: "1:1002500513104:web:5193906c42e30778cee055"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
