7c5caca8d46f0c1b2eb77c128fd9044f476daf74
Christopher Carter <S551122@nwmissouri.edu>
2026-02-19 14:37:50 -0600

    change_password backend code

---
+++ server/Routes/Authentication/change_password.js
@@ -0,0 +1,38 @@
+const bcrypt = require('bcryptjs');
+const User = require('../../Models/user');
+
+async function changePassword(req, res) {
+    const { currentPassword, newPassword } = req.body;
+    const userId = req.user.userId; // Get user ID from the authenticated token
+
+    try {
+        // Find the user in the database
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({message: 'User not found' });
+        }
+        // Check if the current password is correct
+        const passMatch = await bcrypt.compare(currentPassword, user.Password);
+        if (!passMatch) {
+            return res.status(400).json({message: 'Current password is incorrect' });
+        }
+
+        const passpattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+        if (!passpattern.test(newPassword)) {
+            console.log(newPassword)
+            return res.status(400).json({message: 'Password must be at least 8 characters long and include at least one uppercase letter, one lowercase letter, one number, and one special character' });
+        }
+
+        // Hash the new password
+        const hashedNewPassword = await bcrypt.hash(newPassword, 10);
+        // Update the user's password in the database
+        user.Password = hashedNewPassword;
+        await user.save();
+        return res.status(200).json({ message: 'Password changed successfully' });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ message: 'Internal server error' });
+    }
+}
+
+module.exports = changePassword;
