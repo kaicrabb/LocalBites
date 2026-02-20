365dc1ff8b96dc6f87ae9d31ed458be09acf0ffe
Christopher Carter <S551122@nwmissouri.edu>
2026-02-20 10:23:09 -0600

    Delete account backend

---
+++ server/Routes/Authentication/delete_account.js
@@ -0,0 +1,29 @@
+const User = require('../../Models/user');
+const bcrypt = require('bcryptjs');
+
+async function deleteAccount(req, res) {
+    const userId = req.user.userId; // Get user ID from the authenticated token
+    const password = req.body.password; // Get password from the request body
+    try {
+        // Find the user in the database
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({message: 'User not found' });
+        }
+        // Delete the user from the database
+        const passMatch = await bcrypt.compare(password, user.Password);
+        if (passMatch){
+            await User.findByIdAndDelete(userId);
+            return res.status(200).json({ message: 'Account deleted successfully' });
+        } 
+        else {
+            return res.status(400).json({message: 'Password is incorrect' });
+        }
+    } 
+    catch (err) {
+        console.error(err);
+        return res.status(500).json({ message: 'Internal server error' });
+    }
+}
+
+module.exports = deleteAccount;
