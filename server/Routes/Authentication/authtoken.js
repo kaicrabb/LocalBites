4c20a9a5fb37ab14dc1d4f42610cc3e4b3616ed2
Christopher Carter <S551122@nwmissouri.edu>
2026-02-20 12:36:18 -0600

    continued testing of delete account issues

--- server/Routes/Authentication/authtoken.js
+++ server/Routes/Authentication/authtoken.js
@@ -2,15 +2,18 @@ const jwt = require('jsonwebtoken'); // Import jsonwebtoken for tokenization
 const SECRET_KEY = process.env.SECRET_KEY;
 
 const authenticateToken = (req, res, next) => {
+    console.log("AUTH HEADER:", req.headers.authorization);
     const token = req.headers['authorization']?.split(' ')[1];  // Extract token from the Authorization header
   
     if (!token) {
+        console.log("NO TOKEN");
         return res.sendStatus(401);  // No token, unauthorized
     }
 
     jwt.verify(token, SECRET_KEY, (err, user) => {
         if (err) {
-            return res.sendStatus(403);
+            console.log("JWT ERROR:", err.message);
+            return res.sendStatus(403).json({ message: "Forbidden" });
         }
         req.user = user;  // Store user info in the request object
         next();  // Proceed to the next middleware or route handler
