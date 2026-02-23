00e9629147ca71ff1a42bba19f92cb8bf56d4e42
Christopher Carter <S551122@nwmissouri.edu>
2026-02-23 10:17:48 -0600

    Serverside logs

--- server/Routes/Authentication/change_password.js
+++ server/Routes/Authentication/change_password.js
@@ -9,17 +9,19 @@ async function changePassword(req, res) {
         // Find the user in the database
         const user = await User.findById(userId);
         if (!user) {
+            console.log(userId);
             return res.status(404).json({message: 'User not found' });
         }
         // Check if the current password is correct
         const passMatch = await bcrypt.compare(currentPassword, user.Password);
         if (!passMatch) {
+            console.log("Incorrect original password");
             return res.status(400).json({message: 'Current password is incorrect' });
         }
 
         const passpattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
         if (!passpattern.test(newPassword)) {
-            console.log(newPassword)
+            console.log("New password does not meet complexity requirements");
             return res.status(400).json({message: 'Password must be at least 8 characters long and include at least one uppercase letter, one lowercase letter, one number, and one special character' });
         }
 
@@ -28,6 +30,7 @@ async function changePassword(req, res) {
         // Update the user's password in the database
         user.Password = hashedNewPassword;
         await user.save();
+        console.log("Password changed successfully");
         return res.status(200).json({ message: 'Password changed successfully' });
     } catch (err) {
         console.error(err);
