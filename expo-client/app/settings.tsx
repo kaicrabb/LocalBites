1c106e140656938ffee54a7badc8e445d6c42d96
Hayden Bindel <124805187+hbindel26@users.noreply.github.com>
2026-04-07 15:16:06 -0500

    so far what I have
    havent checked if it works

--- expo-client/app/settings.tsx
+++ expo-client/app/settings.tsx
@@ -4,7 +4,11 @@ import Button from './Button';
 import ImageViewer from './ImageViewer';  
 import * as SecureStore from 'expo-secure-store'; 
 import * as ImagePicker from 'expo-image-picker';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { ref, listAll, getDownloadURL, uploadBytes } from "firebase/storage";
+import { storage, auth } from "../../config/firebaseConfig";
+import { onAuthStateChanged, User } from 'firebase/auth';
+import { get } from 'react-native/Libraries/TurboModule/TurboModuleRegistry';
 
 const PlaceholderImage = require ('@/assets/images/default.jpg');
 
@@ -28,16 +32,51 @@ export default function App() {
   };
 
   const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
+
+  const [userFirebase, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    if (!userFirebase) return;
+
+    const fetchProfileImage = async () => {
+      const user_id = userFirebase.uid;
+      const profilePicRef = ref(storage, `profile/${user_id}`);
+
+      try {
+        const url = await getDownloadURL(profilePicRef);
+        setSelectedImage(url);
+        await SecureStore.setItemAsync('profileImage', url);
+      } catch (error) {
+        const saved = await SecureStore.getItemAsync('profileImage');
+        if (saved) setSelectedImage(saved);
+        console.log('No profile image found in strorage yet.');
+      }
+    };
+
+    fetchProfileImage();
+  }, [userFirebase]);
+
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ['images'],
       allowsEditing: true,
-      quality: 1,
+      quality: 0.5,
     });
-    if (!result.canceled) {
-      setSelectedImage(result.assets[0].uri);
-    } else {
-      console.log('No image selected.');  
+    if (!result.canceled && userFirebase) {
+      const imageUri = result.assets[0].uri;
+      try {
+        const response = await fetch(imageUri);
+        const blob = await response.blob();
+        const storageRef = ref(storage, `profile/${userFirebase}`);
+        await uploadBytes(storageRef, blob);
+        const url = await getDownloadURL(storageRef);
+        setSelectedImage(url);
+        await SecureStore.setItemAsync('profileImage', url);
+        alert('Profile picture updated successfully!');
+      } catch (error) {
+        console.error('Error uploading profile picture:', error);
+        alert('Failed to update profile picture. Please try again.');
+      }
     }
   };
 
