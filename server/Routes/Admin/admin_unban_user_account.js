d9ac490f6b32007bc480da6bec69cac9d085a336
Christopher Carter <S551122@nwmissouri.edu>
2026-04-07 15:03:51 -0500

    set up starting backend admin files

---
+++ server/Routes/Admin/admin_unban_user_account.js
@@ -0,0 +1,29 @@
+const bans = require('../../Models/bans');
+const user = require('../../Models/user');
+
+async function adminUnbanUser(req, res) {
+    const userIdToUnban = req.body.userId; //get user id from the request body
+
+    if (!req.user.IsAdmin) {// check that the user is an admin
+        console.log('Unauthorized to unban this user account:', userIdToUnban, 'by user:', req.user.id);
+        return res.status(403).json({ message: 'Not authorized to unban this user account' });
+    }
+    try {
+        //checking if user exists
+        const userToUnban = await user.findById(userIdToUnban);
+        if (!userToUnban) {
+            console.log('User not found:', userIdToUnban);
+            return res.status(404).json({ message: 'User not found' });
+        }
+        //unbanning the user
+        await bans.findOneAndDelete({ userId: userIdToUnban });
+        console.log('User account unbanned successfully by admin:', userIdToUnban);
+        return res.status(200).json({ message: 'User account unbanned successfully' });
+    }
+    catch (err) {
+        console.error(err);
+        return res.status(500).json({ message: 'Internal server error' });
+    }
+}
+
+module.exports = adminUnbanUser;
