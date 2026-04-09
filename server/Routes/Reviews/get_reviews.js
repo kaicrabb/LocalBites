841e2e3d31726c501b72cbad04bf5255df2e3701
Christopher Carter <S551122@nwmissouri.edu>
2026-04-09 14:02:11 -0500

    add console logs to get_reviews

--- server/Routes/Reviews/get_reviews.js
+++ server/Routes/Reviews/get_reviews.js
@@ -5,19 +5,21 @@ const Review = require('../../Models/reviews');
 
 async function getReviews(req, res) {
     try {
+        console.log('Fetching reviews for query parameters:', req.query);
         const { placeId, userId } = req.query
         if (placeId) {
             const reviews = await Review.find({ Place: placeId }).populate('User', 'Username Email');
+            console.log(`Found ${reviews.length} reviews for placeId ${placeId}`);
             return res.status(200).json({ reviews });
         }
         else if (userId){
             const reviews = await Review.find({ User: userId }).populate('Place');
+            console.log(`Found ${reviews.length} reviews for userId ${userId}`);
             return res.status(200).json({ reviews });
         }
         else {
             return res.status(400).json({ message: 'placeId or UserId parameter is required' });
         }
-        res.status(200).json({ reviews });
     } catch (err) {
         console.error('Error fetching reviews:', err);
         res.status(500).json({ message: 'Server error' });
